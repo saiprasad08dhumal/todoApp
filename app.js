1b2b60295f03b9f1f73ed5dd74fffe530a0c44c2
saiprasad08dhumal <saiprasad08dhumal@gmail.com>
2016-06-01 23:27:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,67 +12,87 @@ app.controller('todoCtrl', function($scope, $http) {
         }
     
         $scope.dateChange = function(){
-        $scope.todoList = [];
-        $scope.dateMysql = $scope.formatMysqlDate($scope.date.value);
+            $scope.todoList = [];
+            $scope.dateMysql = $scope.formatMysqlDate($scope.date.value);
 
-        angular.forEach($scope.AllTasks, function(x){
-            if($scope.dateMysql == x.date){
-                $scope.todoList.push(x)
-             //   alert(x.date+" added"+$scope.dateMysql);
-            }
-            else{
-            //     alert(x.date+" not added"+$scope.dateMysql);
-            }
-        });
-    }
-    $http.get("getTask.php")
-    .then(function(response) {
-        $scope.AllTasks = response.data;
+            angular.forEach($scope.AllTasks, function(x){
+                if($scope.dateMysql == x.date){
+                    $scope.todoList.push(x)
+                //   alert(x.date+" added"+$scope.dateMysql);
+                }
+                else{
+                //     alert(x.date+" not added"+$scope.dateMysql);
+                }
+            });
+        }
+
+        $scope.getIssues = function(){
+
+            $http.get("getTask.php")
+            .then(function(response) {
+                $scope.AllTasks = response.data;
 //                $scope.todoList = response.data;
-//            angular.forEach($scope.todoList, function(x) {
+//                angular.forEach($scope.todoList, function(x) {
 //                if (x.done)
                     $scope.TaskNotDone = 'pending <span class="glyphicon glyphicon-question-sign"></span>';
 //                else
                     $scope.TaskDone = 'done <span class="glyphicon glyphicon-ok"></span>';
 //            });
-            $scope.dateChange();
+                $scope.dateChange();
 //            if()
-//    alert(response.data);
-         });
-         
+//        alert(response.data);
+            });
+        }
+        $scope.getIssues();        
     $scope.todoAdd = function() {
-//        $scope.todoList.push({issueName:$scope.todoInput, done:false, date: 0, time:0});
+//        $scope.todoList.push({issueName:$scope.todoInput, done:false, date: $scope.dateMysql, time:$scope.time});
 //          var myEl = angular.element( document.querySelector( '#bodyId' ) );
 //          myEIl.html("wait");
 //            document.getElementById("#bodyId").innerHTML = "wait...";
-            var time1 = $scope.time;
+
 //            alert(time1);
 //            alert($scope.dateMysql);
-
-            $http.post('CreateUpdateDelete.php', {op:'create',issueName: $scope.todoInput, done:false, date: $scope.dateMysql, time: time1})
+  //          $("#Tasktbl").css("display:none");
+            $http.post('CreateUpdateDelete.php', {op:'create',issueName: $scope.todoInput, done:false, date: $scope.dateMysql, time: $scope.time})
                     .success(function(data) {
   //                      alert("create read updated:"+data);
-                        location.reload();
+//                        location.reload();
+                        $scope.getIssues();
+//                        $("#Tasktbl").fadeIn();
+                        
                    });
         $scope.todoInput = "";
     };
     $scope.done = function(mIndex1) {
-              //ajax code to update database
-                
+//                $("#btn"+mIndex1).html($scope.TaskDone);
+//            $("#Tasktbl").fadeout();
+                //ajax code to update database
                 $http.post('CreateUpdateDelete.php', {op:'update',mIndex: mIndex1})
                     .success(function(data) {
-//                        alert("create read updated:"+data);
-
-                        location.reload();
+  //                      alert("create read updated:"+data);
+//                        location.reload();
+                          $scope.getIssues();
+//                          $("#Tasktbl").fadeIn();
                     });
     }
     $scope.remove = function(mIndex1) {
                 //ajax code to remove from database
+//                angular.forEach($scope.todoList, function(x){
+//                            if(mIndex1 == x.mIndex){
+//                                $scope.todoList.pop(x);
+////                                 alert(x.issueName+" deleted");
+//                            }
+//                            else{
+//  //                               alert(x.issueName+" not deleted");
+//                            }
+//                        });                
+//            $("#Tasktbl").fadeout();
                 $http.post('CreateUpdateDelete.php', {op:'delete',mIndex: mIndex1})
                     .success(function(data) {
-//                        alert("create read updated:"+data);
-                        location.reload();
+//                      alert("create read updated:"+data);
+//                      location.reload();     
+                        $scope.getIssues();
+//                         $("#Tasktbl").fadeIn();
                     });
             }
-
 });
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-sanitize.js"></script>
 <script src="app.js" > </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="main.css">
 <body ng-app="myApp" ng-controller="todoCtrl" id="bodyId">
@@ -14,28 +14,43 @@
 <form ng-submit="todoAdd()">
 <br>
 <input type="date"  id="datePicker" ng-model="date.value" placeholder="yyyy-MM-dd" ng-change="dateChange()"  required /><br><br>
+<table class="table table-striped table-bordered table-hover" id="Tasktbl">
+<thead>
+      <tr>
+        <th>Time</th>
+        <th>Task</th>
+        <th>status</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr ng-repeat="x in todoList | orderBy: 'time'">
 
-<div ng-repeat="x in todoList | orderBy: 'time'">
  <!--   <input type="text" id="indexValue" style="//display: none;" ng-model="x.mIndex">
     
     <input type="checkbox" ng-model="x.done"> -->
-    <span ng-bind="x.time"></span>    
-    <span ng-bind="x.issueName"></span> 
+            
+            <td><span ng-bind="x.time"></span> </td>   
+            <td><span ng-bind="x.issueName"></span> </td>
 
  
-
+            <td>
     <div type="button" class="btn btn-default btn-sm" id="btn{{x.mIndex}}" ng-click="done(x.mIndex)" ng-bind-html="x.done ==1?TaskDone:TaskNotDone"> 
     </div>
-    
+    </td>
+    <td>
     <button type="button" class="btn btn-default btn-sm" ng-click="remove(x.mIndex)">
           <span class="glyphicon glyphicon-remove"></span> delete 
         </button>
-    <br><br>
-</div>
+        </td>
+    </tr>
+    </tbody>
+  </table>
+
 <br>
 
     
-    <input type="text" ng-model="todoInput" size="25" placeholder="New appointment"style="margin: 10px;">
+<input type="text" ng-model="todoInput" size="25" placeholder="New appointment"style="margin: 10px;" required> at:
     <input type="text" id="timeId" pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" title="12:34:56"  ng-model="time" placeholder="hh:mm:ss" min="00:00:00" max="23:59:59" >
     <input type="submit">
 
